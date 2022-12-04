7235e951ed69c1b5cc767417843136027390f106
RuleRabbit <89403421+KristianMaelan@users.noreply.github.com>
2022-12-04 21:15:31 +0100

    bruh

--- Kristian/Hash Tables/HashTable.cpp
+++ Kristian/Hash Tables/HashTable.cpp
@@ -1,1 +1,73 @@
 ﻿#include "HashTable.h"
+
+HashTable::HashTable()
+{
+	Table = new LinkedList[TableLength];
+
+}
+
+HashTable::~HashTable()
+{
+	delete[] Table;
+}
+
+//Basic hash function, will probably have collisions and get slowed down. 
+int HashTable::hash(std::string ItemID)
+{
+	int HashValue = 0;
+	for (int i = 0;i < ItemID.length(); i++)
+	{
+		HashValue += ItemID[i];
+	}
+
+	HashValue = (ItemID.length() * HashValue) % TableLength;
+
+	return HashValue;
+}
+
+void HashTable::AddItem(Item* newItem)
+{
+	//Decides placement in the Table after rules set in HASH function.
+	int placement = hash(newItem->Value);
+
+	//Adds item in the linked list located at "placement", and calls the add function in that linked list
+	Table[placement].insertItem(newItem);
+
+}
+
+Item* HashTable::getItem(std::string ItemID)
+{
+	int placement = hash(ItemID);
+
+	return Table[placement].returnItem(ItemID);
+
+}
+
+void HashTable::PrintTable()
+{
+	std::cout << "Printing hash table: " << std::endl;
+
+	for (int i = 0; i < TableLength; i++)
+	{
+		std::cout << "Row nr. " << i + 1 << ": ";
+		Table[i].PrintList();
+	}
+}
+
+int HashTable::GetItemAmount()
+{
+	int ItemAmount = 0;
+
+	for (int i = 0; i < TableLength; i++)
+	{
+		ItemAmount += Table[i].GetListLength();
+	}
+	return ItemAmount;
+}
+
+bool HashTable::removeItem(std::string ItemID)
+{
+
+	return false;
+}
+
--- Kristian/Hash Tables/LinkedList.cpp
+++ Kristian/Hash Tables/LinkedList.cpp
@@ -1,7 +1,135 @@
 ﻿#include "LinkedList.h"
 
-template <typename T>
-LinkedList<T>::LinkedList()
+
+
+LinkedList::LinkedList()
 {
+	Head = new Item;
+	Head->next = nullptr;
+
+	length = 0;
 
 }
+
+
+void LinkedList::insertItem(Item* newItem)
+{
+
+	if (!Head->next)
+	{
+		Head->next = newItem;
+		length++;
+
+		return;
+	}
+
+	//Cycles through until the end to set last item as new item.
+	Item* pre = Head;
+	Item* aft = Head;
+	while (aft)
+	{
+		pre = aft;
+		aft = pre->next;
+	}
+	pre->next = newItem;
+	newItem->next = nullptr;
+
+	length++;
+
+}
+
+
+bool LinkedList::removeItem(std::string Value)
+{
+	if (!Head->next)
+	{
+		return false;
+	}
+	Item* pre = Head;
+	Item* aft = Head;
+	//cycles through list looking for the specific item, if found, delete
+	while (aft)
+	{
+		if (aft->Value == Value)
+		{
+			pre->next = aft->next;
+			delete aft;
+			length--;
+			return true;
+		}
+		pre = aft;
+		aft = pre->next;
+	}
+	return false;
+}
+
+
+Item* LinkedList::returnItem(std::string Value)
+{
+	Item* pre = Head;
+	Item* aft = Head;
+	while (aft)
+	{
+		pre = aft;
+
+		if ((pre != Head) && (pre->Value == Value))
+		{
+			return pre;
+		}
+		aft = pre->next;
+	}
+	return nullptr;
+}
+
+int LinkedList::GetListLength()
+{
+	return length;
+}
+
+
+LinkedList::~LinkedList()
+{
+	Item* pre = Head;
+	Item* aft = Head;
+
+	while (aft)
+	{
+		pre = aft;
+
+		aft = pre->next;
+		if (aft)
+		{
+			delete pre;
+		}
+	}
+}
+
+void LinkedList::PrintList()
+{
+	Item* pre = Head;
+	Item* aft = Head;
+
+	std::cout << std::endl;
+
+	while (aft)
+	{
+		pre = aft;
+
+		if (pre != Head)
+		{
+			std::cout << pre->Value;
+
+			if (pre->next)
+			{
+				std::cout << " - ";
+			}
+			else
+			{
+				std::cout << " ";
+			}
+		}
+		aft = pre->next;
+	}
+	std::cout << std::endl;
+}
+
