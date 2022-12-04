465ec89646b2c67092f6f31b466df9317836e161
Joakim Askeland <89452981+JoakimAskeland@users.noreply.github.com>
2022-12-04 15:22:33 +0100

    Added code

--- Joakim/Queue.cpp
+++ Joakim/Queue.cpp
@@ -9,7 +9,31 @@ using namespace std;
 // FIFO: First In First Out		||		FCFS: First Come First Served
 //
 
+void printQueue(queue<int> queue)
+{
+	while (!queue.empty())
+	{
+		cout << queue.front() << " ";
+		queue.pop();
+	}
+	cout << endl;
+}
 
+int main()
+{
+	queue<int> myQueue;
+
+	myQueue.push(1);
+	myQueue.push(2);
+	myQueue.push(3);
+
+	cout << "Size is " << myQueue.size() << endl;
+	cout << "First element is " << myQueue.front() << endl;
+	cout << "Last element is " << myQueue.back() << endl;
+
+	cout << "My queue: " << endl;
+	printQueue(myQueue);
+}
 
 
 
