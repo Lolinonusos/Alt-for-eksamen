8aef4e47d6ce14b9d08814bc9537c9e636ce44bc
Joakim Askeland <89452981+JoakimAskeland@users.noreply.github.com>
2022-12-04 16:50:12 +0100

    La til doublyLinkedList

--- Joakim/LinkedList.cpp
+++ Joakim/LinkedList.cpp
@@ -73,3 +73,98 @@ int main()
 
 }
 */ 
+
+// Doubly linked list -------------------------------------------------------------------------------------------------------------------------------------------------
+
+// video: https://www.youtube.com/watch?v=-StYr9wILqo&t=623s
+
+// Can be traversed in two directions (forward and backwards). 
+
+// For first node:
+//	1. Create new node
+//	2. Set node value
+//	3. Set node->next = nullptr and node->previous = nullptr
+//	4. head = node and tail = node
+//
+
+/* 
+
+class Node
+{
+public:
+	int value;
+	Node* next;
+	Node* previous;
+};
+
+void printForward(Node* head)
+{
+	Node* traverser = head;
+
+	while (traverser != nullptr)
+	{
+		cout << traverser->value << endl;
+
+		traverser = traverser->next;
+	}
+}
+
+void printBackwards(Node* tail)
+{
+	Node* traverser = tail;
+
+	while (traverser != nullptr)
+	{
+		cout << traverser->value << endl;
+
+		traverser = traverser->previous;
+	}
+}
+
+int main()
+{
+	Node* head;
+	Node* tail;
+
+	// Add first node
+	Node* node = new Node();
+
+	node->value = 4;
+	node->next = nullptr;
+	node->previous = nullptr;
+
+	head = node;
+	tail = node;
+
+	// Add second node
+	node = new Node();
+	node->value = 5;
+	node->next = nullptr;
+	node->previous = tail;
+	tail->next = node;
+	tail = node;
+
+	// Add third node
+	node = new Node();
+	node->value = 6;
+	node->next = nullptr;
+	node->previous = tail;
+	tail->next = node;
+	tail = node;
+
+	// Add fourth node
+	node = new Node();
+	node->value = 7;
+	node->next = nullptr;
+	node->previous = tail;
+	tail->next = node;
+	tail = node;
+
+	printForward(head);
+
+	printBackwards(tail);
+
+	return 0;
+}
+
+*/ 
